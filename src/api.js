b7f8dac5c14386cf22294f84b08e880a9495042c
Ellen Harris <ellenharris@Ellens-MacBook-Air.local>
2022-03-10 11:07:10 +0000

    hopefully completed voting functionality, but need to merge previous branch first

--- src/api.js
+++ src/api.js
@@ -15,6 +15,8 @@ export const fetchArticles = (topic) => {
     });
 };
 
-export const updateVotes = (change) => {
-  return newsApi.patch('/articles/:article_id/comments', { inc_votes: change });
+export const updateVotes = (article_id, change) => {
+  return newsApi.patch(`/articles/${article_id}/comments`, {
+    inc_votes: change,
+  });
 };
